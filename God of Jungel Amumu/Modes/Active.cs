e3e7386b91136e4d9ea25f714d93313bf5bb5759
Taazuma <multivita@ok.de>
2016-06-26 22:55:17 +0200

    Add files via upload

---
+++ God of Jungel Amumu/Modes/Active.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Constants;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using static Eclipse.SpellsManager;
+using static Eclipse.Menus;
+using System.Diagnostics;
+
+namespace Eclipse.Modes
+{
+    internal class Active
+    {
+        public static Obj_AI_Minion Minion;
+
+        public static void Execute()
+        {
+            if (Smite.IsLearned)
+              { 
+            Minion = (Obj_AI_Minion)EntityManager.MinionsAndMonsters.Monsters.FirstOrDefault(buff => Program._player.IsInRange(buff, 570) && (buff.Name.StartsWith(buff.BaseSkinName) || Program.BuffsThatActuallyMakeSenseToSmite.Contains(buff.BaseSkinName)) && !buff.Name.Contains("Mini") && !buff.Name.Contains("Spawn"));
+            AIHeroClient target = TargetSelector.GetTarget(570, DamageType.Magical);
+
+            if (MiscMenu.GetKeyBindValue("smitekey") && Minion.IsValidTarget(570) && Minion.Health < Program.SmiteDmgMonster(Minion) && MiscMenu.GetCheckBoxValue("sjgl") && SpellsManager.Smite.IsReady())
+            {
+                Smite.Cast(Minion);
+            }
+
+            if (target.IsValidTarget(570) && target.Health < Program.SmiteDmgHero(target) && MiscMenu.GetCheckBoxValue("sks") && SpellsManager.Smite.IsReady())
+            {
+                Smite.Cast(target);
+            }
+
+              }
+
+            //W autodisable thanks to Sunnyline2
+            if (MiscMenu.GetCheckBoxValue("smartW")&& Program.WStatus())
+            {
+                int monsters = EntityManager.MinionsAndMonsters.CombinedAttackable.Where(monster => monster.IsValidTarget(W.Range * 2)).Count();
+                int enemies = EntityManager.Heroes.Enemies.Where(enemy => enemy.IsValidTarget(W.Range * 2)).Count();
+                if (monsters == 0 && enemies == 0)
+                    Program.WDisable();
+            }
+            //// Sunnyline2
+
+
+            //////////////////// KS Q
+            var targetKSQ = TargetSelector.GetTarget(SpellsManager.Q.Range, DamageType.Magical);
+
+            if (targetKSQ != null && KillStealMenu.GetCheckBoxValue("qUse") && SpellsManager.Q.IsReady())
+            {
+                var predQ2 = SpellsManager.Q.GetPrediction(targetKSQ);
+                if (predQ2.HitChance >= HitChance.High && targetKSQ.Health < Player.Instance.GetSpellDamage(targetKSQ, SpellSlot.Q))
+                {
+                    SpellsManager.Q.Cast(predQ2.CastPosition);
+                    return;
+                }
+            }//////////////////// END KS Q
+
+
+            //////////////////// KS Q Logic #2
+            if (KillStealMenu.GetCheckBoxValue("qUse"))
+            {
+                var qtarget = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+                if (qtarget == null) return;
+
+                if (Q.IsReady())
+                {
+                    var qDamage = qtarget.GetDamage(SpellSlot.Q);
+
+                    var predictedHealth = Prediction.Health.GetPrediction(qtarget, Q.CastDelay + Game.Ping);
+
+                    if (predictedHealth <= qDamage && Q.GetPrediction(qtarget).HitChance >= Hitch.hitchance(Q, FirstMenu))
+                    {
+                        var rangi = TargetSelector.GetTarget(Program._player.GetAutoAttackRange(), DamageType.Physical);
+                        if (qtarget.IsValidTarget(Q.Range))
+                        {
+                            Q.Cast(qtarget);
+                        }
+                    }
+                }
+            }            //////////////////// END Logic #2
+
+
+        }
+
+    }
+}
---
+++ God of Jungel Amumu/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Constants;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using static Eclipse.SpellsManager;
+using static Eclipse.Menus;
+using Eclipse.Modes;
+using EloBuddy.SDK.Menu;
+
+namespace Eclipse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+        public static AIHeroClient _player
+        {
+            get { return ObjectManager.Player; }
+
+        }
+
+        public const float SmiteRange = 570;
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            //Put the name of the champion here
+            if (Player.Instance.ChampionName != "Amumu") return;
+            SpellsManager.InitializeSpells();
+            DrawingsManager.InitializeDrawings();
+            Events.Initialize();
+            Menus.CreateMenu();
+            ModeManager.InitializeModes();
+        }
+
+        public static float SmiteDmgMonster(Obj_AI_Base target)
+        {
+            return Player.Instance.GetSummonerSpellDamage(target, DamageLibrary.SummonerSpells.Smite);
+        }
+
+        public static float SmiteDmgHero(AIHeroClient target)
+        {
+            return Player.Instance.CalculateDamageOnUnit(target, DamageType.True,
+                20.0f + Player.Instance.Level * 8.0f);
+        }
+
+
+        public static readonly string[] BuffsThatActuallyMakeSenseToSmite =
+       {
+                "SRU_Red", "SRU_Blue", "SRU_Dragon_Water",  "SRU_Dragon_Fire", "SRU_Dragon_Earth", "SRU_Dragon_Air", "SRU_Dragon_Elder",
+                "SRU_Baron", "SRU_RiftHerald", "TT_Spiderboss",
+       };
+
+        public readonly static string[] MonstersNames =
+        {
+            "SRU_Dragon_Water", "SRU_Dragon_Fire", "SRU_Dragon_Earth", "SRU_Dragon_Air", "SRU_Dragon_Elder", "Sru_Crab", "SRU_Baron", "SRU_RiftHerald",
+            "SRU_Red", "SRU_Blue",  "SRU_Krug", "SRU_Gromp", "SRU_Murkwolf", "SRU_Razorbeak",
+            "TT_Spiderboss", "TTNGolem", "TTNWolf", "TTNWraith",
+        };
+
+        public static bool getCheckBoxItem(Menu m, string item)
+        {
+            return m[item].Cast<CheckBox>().CurrentValue;
+        }
+
+        public static int getSliderItem(Menu m, string item)
+        {
+            return m[item].Cast<Slider>().CurrentValue;
+        }
+
+        public static bool getKeyBindItem(Menu m, string item)
+        {
+            return m[item].Cast<KeyBind>().CurrentValue;
+        }
+
+        public static bool WStatus()
+        {
+            if (Player.HasBuff("AuraofDespair"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void WEnable()
+        {
+            if (!WStatus())
+                SpellsManager.W.Cast();
+            return;
+        }
+
+        public static void WDisable()
+        {
+            if (WStatus())
+                SpellsManager.W.Cast();
+            return;
+        }
+
+    }
+}
